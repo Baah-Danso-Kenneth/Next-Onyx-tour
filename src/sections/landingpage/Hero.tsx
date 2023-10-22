95b970169fd2ae423f81b834f17f68623372ea86
Kenneth <kwadwobaahdanso@gmail.com>
2023-10-22 22:44:57 +0000

    additional images added to complement footer section

---
+++ src/sections/landingpage/Hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeroImage from '../../../public/assets/images/hero_image.png'
+import Image from 'next/image';
+
+function Hero() {
+  return (
+    <div className='hero-container'>
+      <div className="hero-section-overlay"></div>
+      <div className="hero-image-section">
+        <Image className='img-img' src={HeroImage} alt="source"/>
+      </div>
+      <div className="hero-statement">
+        <p>Change awaits in the Journey and destination</p>
+      </div>
+      </div>
+  )
+}
+
+export default Hero
