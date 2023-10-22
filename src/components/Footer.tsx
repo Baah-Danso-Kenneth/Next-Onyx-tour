d0ae975cca86d89dbb9ab1493c01c47e90be0c8e
Kenneth <kwadwobaahdanso@gmail.com>
2023-10-22 21:50:35 +0000

    footer structure added to project with sponsors image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-        Footer
+    <div className='footer-container'>
+        <div className="sponsors">
+           <img/>
+           <img/>
+           <img/> 
+        </div>
+
+        <div className="footer-comment">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, atque tempore eum dignissimos maiores ipsam nostrum quo, ut cumque laudantium maxime dolore. Architecto eveniet tempora explicabo quia, reprehenderit ducimus pariatur!</p>
+        </div>
+
+        <div className="contact-us">
+          <h3>Contact us</h3>
+           <h4>ony-ghana@tour.com</h4>
+           <p>Monday-Friday: 10am - 6pm WST</p>
+        </div>
+
+        <div className="footer-tag">
+          <p> &copy; 2024 Onyx Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, possimus!</p>
+        </div>
     </div>
   )
 }
