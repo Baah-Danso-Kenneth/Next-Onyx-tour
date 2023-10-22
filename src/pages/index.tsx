9923a7aa6c550e090423cd8f35e126776aa689df
Kenneth <kwadwobaahdanso@gmail.com>
2023-10-22 18:46:03 +0000

    what we do jsx structure created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Layout from '@/components/Layout'
 import Hero from '@/sections/landingpage/Hero'
+import WhatWeDo from '@/sections/landingpage/WhatWeDo'
 import React from 'react'
 
 function index() {
   return (
     <Layout>
       <Hero/>
+      <WhatWeDo/>
     </Layout>
   )
 }
