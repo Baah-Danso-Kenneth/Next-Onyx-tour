75490c047de80df6b7a1071d15f011e284dd0e6e
Kenneth <kwadwobaahdanso@gmail.com>
2023-10-23 11:15:16 +0000

    handling responsiveness and styling laptop screens

--- src/components/NewsLetter.tsx
+++ src/components/NewsLetter.tsx
@@ -6,19 +6,23 @@ function NewsLetter() {
     <div className='news-letter-container'>
         <div className="banner-text">
             <h3>Lorem ipsum dolor sit amet.</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, rerum.</p>
+            <p className='small-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, rerum.</p>
         </div>
-      <div className="news-letter-container-names">
-        <input/>
-        <input/>
-      </div>
-      <div className="news-letter-container-email">
-        <input/>
-      </div>
-      <div className="new-letter-btn">
-        <Button text='join' link='#'/>
-      </div>
-      <p>Lorem ipsum dolor sit amet.</p>
+
+        <div className="flex-input">
+           <div className="flex-input-names">
+              <input type='text' placeholder='First Name'/>
+               <input type='text' placeholder='Last Name'/>
+          </div>
+
+           <div className="flex-input-email">
+               <input type='text' placeholder='Email Address'/>
+             </div>
+      <div className="flex-input-btn">
+            <button type='submit'>join now</button>
+        </div>
+          </div>
+      <p className='small-text-2'>Lorem ipsum dolor sit amet.</p>
     </div>
   )
 }
