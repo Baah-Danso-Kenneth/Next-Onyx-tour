8aa7ec3d8f003b7d715945186453284b6e8c08a9
Kenneth <kwadwobaahdanso@gmail.com>
2023-10-23 19:35:31 +0000

    recommend component: we reuse the card component in completing this code, also labtop responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from '@/components/Layout'
 import Hero from '@/sections/landingpage/Hero'
 import KindWords from '@/sections/landingpage/KindWords'
+import Recommend from '@/sections/landingpage/Recommend'
 import WhatWeDo from '@/sections/landingpage/WhatWeDo'
 import WhereWeGo from '@/sections/landingpage/WhereWeGo'
 import React from 'react'
@@ -11,6 +12,7 @@ function index() {
       <Hero/>
       <WhatWeDo/>
       <WhereWeGo/>
+      <Recommend/>
       <KindWords/> 
     </Layout>
   )
