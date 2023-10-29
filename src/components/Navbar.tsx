6dc3656b14ad6679a287bbac93e087bdd09ea03c
Kenneth <kwadwobaahdanso@gmail.com>
2023-10-29 20:13:10 +0000

    drop-downs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import CompanyLogo from '../../public/assets/images/campalla.png'
 import {GiHamburgerMenu} from 'react-icons/gi';
@@ -6,22 +6,48 @@ import {CgClose} from 'react-icons/cg'
 
 function Navbar() {
   const [responsive,setResponsive]=useState(false)
+  const [scrollPosition, setScrollPosition]=useState(0)
+  const [scrolled, setScrolled]=useState(false)
+  const [isDestinationHover, setIsDestinationHover]=useState(false)
+  const [isShopHover, setIsShopHover]=useState(false)
+  const [isAboutHover, setIsAboutHover]=useState(false)
+
+
+  useEffect(()=>{
+    function handleScroll(){
+        const scrollY= window.scrollY || document.documentElement.scrollTop
+        setScrollPosition(scrollY)
+        setScrolled(scrollY > 500)
+    }
+    window.addEventListener('scroll', handleScroll);
+
+    return ()=>{
+        window.removeEventListener('scroll', handleScroll)
+    }
+  },[])
+
+
+
   return (
-    <div className='nav-container'>
+    <div className={`nav-container ${scrolled && 'nav-increase'}`}>
         <div className="nav-container-element">
             <div className='right-section'>
                 <p>small group trips</p>
-                <p>destinations</p>
+                <p onMouseEnter={()=>setIsDestinationHover(true)} onMouseLeave={()=>setIsDestinationHover(false)}>destinations</p>
             </div>
 
-            <div className="logo">
+           {scrolled ? " " : (
+             <div className="logo">
                 <Image src={CompanyLogo} alt='company-logo' quality={100}/>
             </div>
 
+           )}
+
+
             <div className='last-section'>
                 <p>custom trips</p>
-                <p>shop</p>
-                <p>about</p>
+                <p onMouseEnter={()=>setIsShopHover(true)} onMouseLeave={()=>setIsShopHover(false)}>shop</p>
+                <p onMouseEnter={()=>setIsAboutHover(true)} onMouseLeave={()=>setIsAboutHover(false)}>about</p>
             </div>
 
             <div className="icon-for-mobile-tablet">
@@ -34,6 +60,56 @@ function Navbar() {
                 <Image src={CompanyLogo} alt="company-logo"/>
             </div>
         </div>
+
+        <div className="hover-state-container">
+
+            {isDestinationHover ?
+              (
+            <div className={`destination ${scrolled && 'element-down-destination'}`}>
+                <ul>
+                    <li>Volta region</li>
+                    <li>Central region</li>
+                    <li>Easter region</li>
+                    <li>Kente Movement</li>
+                    <li>2024 + 2025 Calender</li>
+                </ul>
+            </div>
+              )
+             :
+             ""}
+
+            { isShopHover ?
+                (
+               <div className={`shop ${scrolled && 'element-down-shop'}`}>
+                  <ul>
+                    <li>Laurens pick</li>
+                     <li>Onyx zone</li>
+                  </ul>
+               </div>
+                )
+                :
+                ""
+            }
+
+          {
+            isAboutHover ? 
+             (
+             <div className={`about ${scrolled && " element-down-about" }`}>
+                <ul>
+                    <li>Our story </li>
+                    <li>In the news</li>
+                    <li>faqs</li>
+                    <li>Say hello</li>
+                </ul>
+            </div>
+             )
+             :
+             ""
+          }
+
+
+        </div>
+
     </div>
   )
 }
