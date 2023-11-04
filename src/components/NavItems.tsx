51a2378560752bd1659609b12c6b65d894ce505a
Kenneth <kwadwobaahdanso@gmail.com>
2023-11-04 20:28:00 +0000

    component to hold navbar items

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,83 @@
+export const DestinationItems=[
+    {
+        id:1,
+        title:"Central",
+        path:"#",
+        cName:"nav-item"
+    },
+
+    {
+        id:2,
+        title:"Eastern",
+        path:"#",
+        cName:"nav-item"
+    },
+
+    {
+        id:3,
+        title:"Volta",
+        path:"#",
+        cName:"nav-item"
+    },
+    {
+        id:4,
+        title:"Kente",
+        path:"#",
+        cName:"nav-item"
+    },
+
+    {
+        id:5,
+        title:"Central",
+        path:"#",
+        cName:"nav-item"
+    }
+]
+
+
+export const AboutItem=[
+  {
+    id:1,
+    title:"Our story",
+    path:"/our-story",
+    cName:"nav-item"
+  },
+
+    {
+    id:2,
+    title:"In the News",
+    path:"/in-the-news",
+    cName:"nav-item"
+  },
+
+    {
+    id:3,
+    title:"FAQ'S",
+    path:"/faq",
+    cName:"nav-item"
+  },
+
+    {
+    id:4,
+    title:"Say hello",
+    path:"/say-hello",
+    cName:"nav-item"
+  },
+]
+
+
+export const ShopItems=[
+    {
+      id:1,
+      title:"Onyx Pick",
+      path:"/onyx",
+      cName:"nav-items"
+    },
+
+    {
+      id:2,
+      title:"Laura Shop",
+      path:"/laura",
+      cName:"nav-items"
+    }
+]
