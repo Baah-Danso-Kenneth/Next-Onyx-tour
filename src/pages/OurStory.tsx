783a12b266175271e74a71a89548d1aa8a38a1c4
Kenneth <kwadwobaahdanso@gmail.com>
2023-11-06 19:38:17 +0000

    added loader state Ourstory component

--- src/pages/OurStory.tsx
+++ src/pages/OurStory.tsx
@@ -1,16 +1,33 @@
 import Layout from '@/components/Layout'
+import Loading from '@/components/Loading'
 import Adventure from '@/section/About/OurStory/Adventure'
 import Description from '@/section/About/OurStory/Description'
 import Hero from '@/section/About/OurStory/Hero'
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 function OurStory() {
+  const [loading,setLoading]=useState(false)
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(true)
+    },3000)
+    
+  },[])
   return (
-    <Layout>
+
+    !loading ? (
+      <Loading/>
+    ) : (
+      <>
+       <Layout>
       <Hero/>
       <Description/>
       <Adventure/>
     </Layout>
+    </>
+
+    )
+
   )
 }
 
