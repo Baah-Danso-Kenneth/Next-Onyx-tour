2b6d27996feece01b81a585c94a7562844b3962f
Kenneth <kwadwobaahdanso@gmail.com>
2023-11-08 01:39:09 +0000

    style talk-to-us Component

--- src/section/About/SayHello/Hello.tsx
+++ src/section/About/SayHello/Hello.tsx
@@ -11,56 +11,60 @@ function Hello() {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quo.</p>
             </div>
 
-            <form action="#">
-                <div className="name-content">
-                    <h3>Name (required)</h3>
+            <form>
+                <div>
+                    <div className="name-content">
+                    <h3>Name <span>(required)</span></h3>
                     <div className="first-name-last-name">
                         <div className="first-name">
-                             <label htmlFor="first-name">FirstName</label>
+                             <label htmlFor="first-name">FirstName</label><br/>
                              <input type="text" />
                         </div>
 
                              <div className="last-name">
-                             <label htmlFor="last-name">LastName</label>
+                             <label htmlFor="last-name">LastName</label><br/>
                              <input type="text" />
                         </div>
                        
                     </div>
+                </div>
                     <div className="email">
-                        <label htmlFor="email">Email (required)</label>
+                        <label htmlFor="email">Email <span>(required)</span></label><br/>
                         <input type="text" />
                     </div>
 
                     <div className="subject">
-                        <label htmlFor="subject">Subject</label>
+                        <label htmlFor="subject">Subject</label><br/>
                         <input type="text" />
                     </div>
 
                     <div className="message">
-                        <label htmlFor="message">Message (required)</label>
+                        <label htmlFor="message">Message (required)</label><br/>
                        <textarea id='message' name='message'/>
                     </div>
 
                    <div className="checkbox">
-                    <h1>do you want us to give you a call?</h1>
-                    <label htmlFor="yes">
-                        <input type="checkbox" /> Yes please
-                    </label>
+                    <h3>do you want us to give you a call? <span>(required)</span></h3>
 
+                    <div className="checkbox-item">
+                       <label htmlFor="yes" className="checkbox-label">
+                          <input type="checkbox" id="yes" className="box-checks" /> Yes please
+                        </label>
+                     <label htmlFor="no" className="checkbox-label">
+                      <input type="checkbox" id="no" /> No please, I prefer communication via email only
+                     </label>
+                  </div>
 
-                     <label htmlFor="yes">
-                        <input type="checkbox" /> No please i prefer communication via email only
-                    </label>
 
                    </div>
 
                     <div className="phone">
-                        <label htmlFor="phone">Phone</label>
+                        <label htmlFor="phone">Phone</label><br/>
                         <input type="text" />
                     </div>
 
                     <div className="time-to-reach-out">
-                        <label htmlFor="reach">best time to reach you out</label>
+                        <label htmlFor="reach">best time to reach you out</label><br/>
                         <input type="text" />
                     </div>
 
