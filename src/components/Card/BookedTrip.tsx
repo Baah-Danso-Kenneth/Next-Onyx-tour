bb5fbc8097cb9b9962c07705670043d9131a2dde
Kenneth <kwadwobaahdanso@gmail.com>
2023-11-13 02:38:44 +0000

    adding styles to bookedTrip components

--- src/components/Card/BookedTrip.tsx
+++ src/components/Card/BookedTrip.tsx
@@ -1,21 +1,26 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import React from 'react'
 
 interface BookedTripInterface{
     sold?:string;
     date:string;
-    img:string;
+    img:StaticImageData;
 }
 
 function BookedTrip({sold,date,img}:BookedTripInterface) {
   return (
     <div className='booked-container'>
-        <div className="img">
+        <div className="booked-container-img">
+            <div className="sold">
+                <p>{sold}</p>
+            </div>
             <Image src={img} alt="book-img"/>
         </div>
         <div className="text">
             <p>{date}</p>
-            <button>book now</button>
+            <div className="button-content">
+                <button>book now</button>
+            </div>
         </div>
       
     </div>
--- src/components/WinterTripsContent.tsx
+++ src/components/WinterTripsContent.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import WinterImg1 from '../../public/assets/images/winter-img1.jpg';
+import WinterImg2 from '../../public/assets/images/winter-img2.jpg'
+import WinterImg3 from '../../public/assets/images/winter-img3.jpg'
+import WinterImg4 from '../../public/assets/images/winter-img4.jpg'
+import WinterImg5 from '../../public/assets/images/winter-img5.jpg'
+import WinterImg6 from '../../public/assets/images/winter-img6.jpg'
+import BookedTrip from './Card/BookedTrip';
+
 
 function WinterTripsContent() {
   const bookedInfo=[
-    {id:1, sold:"sold out", date:"Winter trip: November 3 -12, 2023", img:""},
+    {id:1, sold:"sold out", date:"Winter trip: November 3 -12, 2023", img:WinterImg1},
+     {id:2, sold:"sold out", date:"Winter trip: November 3 -12, 2023", img:WinterImg2},
+      {id:3, sold:"sold out", date:"Winter trip: November 3 -12, 2023", img:WinterImg3},
+       {id:4, sold:"sold out", date:"Winter trip: November 3 -12, 2023", img:WinterImg4},
+        {id:5, sold:"sold out", date:"Winter trip: November 3 -12, 2023", img:WinterImg5},
+         {id:6, sold:"sold out", date:"Winter trip: November 3 -12, 2023", img:WinterImg6},
   ]
 
   return (
@@ -14,9 +27,15 @@ function WinterTripsContent() {
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos accusantium aspernatur dolorem sapiente.</p>
               <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur voluptate fugiat unde laboriosam consequatur.</h6>
           </div>
-
-
         </div>
+
+        <div className="trips-date-elements-card">
+             {bookedInfo.map(({sold, img,date,id})=>{
+              return(
+                <BookedTrip key={id} sold={sold} img={img} date={date}/>
+              )
+             })}
+          </div>
 
       </div>
      </div>
